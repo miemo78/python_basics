7300b8bd9a5a099a9454404933ae39d7d66eea04
miemo <keiko.takeda.78@gmail.com>
2021-08-09 13:39:42 +0900

    変数名Lをtextに変更

--- 04/output_list.py
+++ 04/output_list.py
@@ -1,16 +1,16 @@
 sample_list = ["yamada",100,"taro",True]
 
 #このリストの中身は xx と xx と xx です。
-def output_list(L):
-    L=[str(a) for a in L]
-    L="と".join(L)
-    print("このリストの中身は" + L + "です")
+def output_list(text):
+    text=[str(a) for a in text]
+    text="と".join(text)
+    print("このリストの中身は" + text + "です")
 
 #このリストの中身は「xx」と「xx」と「xx」と「xx」です。
-def output_list2(L):
-    L=["「"+str(a)+"」" for a in L]
-    L="と".join(L)
-    print("このリストの中身は" + L + "です")
+def output_list2(text):
+    text=["「"+str(a)+"」" for a in text]
+    text="と".join(text)
+    print("このリストの中身は" + text + "です")
 
 output_list(sample_list)
 output_list2(sample_list)
